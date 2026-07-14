8d2ed6e7f90bf27bc8350fb0cb18d79d8ee3f976
CCCirclekkko <93822468+CCCirclekkko@users.noreply.github.com>
2026-07-14 14:44:17 +0800

    Add screenshot mode for UI previews

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QDir>
+#include <QTimer>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
@@ -8,9 +9,25 @@ int main(int argc, char *argv[])
     QApplication::setApplicationName(QStringLiteral("ProgramViz"));
     QApplication::setOrganizationName(QStringLiteral("ProgramViz"));
 
-    const QString initialPath = argc > 1 ? QString::fromLocal8Bit(argv[1]) : QDir::currentPath();
+    QString initialPath = QDir::currentPath();
+    QString screenshotPath;
+    for (int index = 1; index < argc; ++index) {
+        const QString argument = QString::fromLocal8Bit(argv[index]);
+        if (argument == QStringLiteral("--screenshot") && index + 1 < argc) {
+            screenshotPath = QString::fromLocal8Bit(argv[++index]);
+        } else if (!argument.startsWith(QStringLiteral("--"))) {
+            initialPath = argument;
+        }
+    }
     MainWindow window(initialPath);
     window.show();
+
+    if (!screenshotPath.isEmpty()) {
+        QTimer::singleShot(250, &app, [&app, &window, screenshotPath]() {
+            window.grab().save(screenshotPath);
+            app.quit();
+        });
+    }
 
     return app.exec();
 }
